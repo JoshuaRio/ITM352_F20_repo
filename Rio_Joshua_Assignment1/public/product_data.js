409a1f168b8fd82cdaa570af9af4136d87468912
Joshua_Rio <jmrio@hawaii.edu>
2020-11-07 07:01:49 -1000

    Updated Assignment1

---
+++ Rio_Joshua_Assignment1/public/product_data.js
@@ -0,0 +1,32 @@
+products = 
+[
+        {
+        "brand" : "Tie-Die",
+        "price" : 44.95,
+        "image" : "https://img.hollisterco.com/is/image/anf/KIC_322-2531-1157-208_prod1?$product-medium-hol$"
+        },
+        {
+        "brand" : "Dri-Fit",
+        "price" : 25.50,
+        "image" : "https://static.augustasportswear.com/product/695HBM_TRR_aws_640.jpg"
+        },
+        {
+        "brand" : "Pullover",
+        "price" : 35.00,
+        "image" : "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/ad6244b8990a473190e2a93800b431f3_9366/essentials-3-stripes-pullover-hoodie.jpg"
+        },
+        {
+        "brand" : "Designer",
+        "price" : 45.00,
+        "image" : "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQPS_F3d98kV2Ntog86W1Sve-yAV-Ds9PgeM8GYeVtxZgPh6UcEe3jzXi3WyrmNDke3JSmts-FTcKc&usqp=CAc"
+        },
+        {
+        "brand" : "Polo",
+        "price" : 75.00,
+        "image" : "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQzbZGkaCv3BZL7h141Ggx3O_hErp3PVrMUDVCI3AdYGFCsL86voesSQHPdReInhvddHnM4puQn0Q&usqp=CAc"
+        }
+    ]
+
+if(typeof module != 'undefined') {
+  module.exports.products = products;
+}
--- Rio_Joshua_Assignment1/server.js
+++ Rio_Joshua_Assignment1/server.js
@@ -1,109 +1,48 @@
-/*Following code is mainly from Dan Port and screencasts from lab 13 */
-
-// To access code from node packages
+//Coded taken and modified from Kazman's info_server_Ex4.js from lab 13
 var express = require('express');
+var app = express();
 var myParser = require("body-parser");
-var products = require('./products.json');
+var data = require('./public/product_data.js');
+var products = data.products;
 var fs = require('fs');
-var app = express(); // express app
+const queryString = require('query-string');
 
-app.use(myParser.urlencoded({ extended: true })); 
+app.use(express.static('./public'));
+app.use(myParser.urlencoded({ extended: true }));
 
-// Response when /process_invoice is requested, when purchase form is submitted
-app.post("/process_invoice", function (request, response, next) { //Need to set to process_form to access invoice
+app.post("/process_quantity_form", function (request, response) {
     let POST = request.body;
-    if(typeof POST['purchase_submit'] != 'undefined') {
-        console.log('No purchase form data');
-        next();
-    } 
+    
+    if (typeof POST['submitPurchase'] != 'undefined') {
+                var hasvalidquantities=true; // assuming that the variable will be true// 
+                var hasquantities=false
+                for (i = 0; i < products.length; i++) {
+                    
+                                qty=POST[`quantity${i}`];
+                                hasquantities=hasquantities || qty>0; // If its value is larger than 0 then it is good//
+                                hasvalidquantities=hasvalidquantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid//     
+                } 
+                // if all quantities are valid, generate the invoice// 
+                const stringified = queryString.stringify(POST);
+                if (hasvalidquantities && hasquantities) {
+                    response.redirect("./invoice.html?"+stringified); // using the invoice.html and all the data that is input//
+                }  
+                else {response.send('Enter a valid quantity!')} 
+            }
+        });
 
-    console.log(Date.now() + ': Purchase made from ip ' + request.ip + ' data: ' + JSON.stringify(POST));
-
-    var contents = fs.readFileSync('./views/invoice.template', 'utf8');
-    response.send(eval('`' + contents + '`')); 
-
-    // Uses function to display invoice
-    function display_invoice_table_rows() {
-        subtotal = 0;
-        str = '';
-        for (i = 0; i < products.length; i++) {
-            qty = 0;
-            val = POST[`quantity${i}`];
-            if(isNonNegInt(val)) {
-                qty = val;
-            }
-            if (qty > 0) {
-                // product row
-                extended_price = qty * products[i].price
-                subtotal += extended_price;
-                str += (`
-      <tr>
-        <td align="center" width="43%">${products[i].name}</td>
-        <td align="center" width="11%">${qty}</td>
-        <td align="center" width="13%">\$${products[i].price}</td>
-        <td align="center" width="54%">\$${extended_price}</td>
-      </tr>
-      `);
-            }
-        }
-        // Compute tax
-        tax_rate = 0.0575;
-        tax = tax_rate * subtotal;
-
-        // Compute shipping
-        if (subtotal <= 50) {
-            shipping = 2;
-        }
-        else if (subtotal <= 100) {
-            shipping = 5;
-        }
-        else {
-            shipping = 0.05 * subtotal; // 5% of subtotal
-        }
-
-        // Compute grand total
-        total = subtotal + tax + shipping;
-        
-        return str;
-    }
-
-});
-
-// Function to check whether a quantity is a non-negative integer
-function isNonNegInt(q, returnErrors = false) {
+function isNonNegInt(stringToCheck, returnErrors = false) {
     errors = []; // assume no errors at first
-    if (Number(q) != q) {
-        errors.push('Not a number!');// Check if string is a number value
-    } else {
-        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-        if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    }
-    return returnErrors ? errors : ((errors.length > 0) ? false : true);
+    if (Number(stringToCheck) != stringToCheck) errors.push('Not a number!'); // Check if string is a number value
+    if (stringToCheck < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if (parseInt(stringToCheck) != stringToCheck) errors.push('Not an integer!'); // Check that it is an integer
+
+    return returnErrors ? errors : (errors.length == 0);
 }
 
-// Response when /store is requested
-app.get("/store", function (request, response) {
-    var contents = fs.readFileSync('./views/display_products.template', 'utf8');
-    response.send(eval('`' + contents + '`')); // render template string
-
-    // Calls on this function to display products in /store page
-    function display_products() {
-        str = '';
-        for (i = 0; i < products.length; i++) {
-            str += `
-                <section class="item">
-                    <h3>${products[i].name}</h3>
-                    <p><img src="${products[i].image}"></p>
-                    <p>$${products[i].price.toFixed(2)}</p>
-                    <p><label id="quantity${i}_label"}">Quantity:</label></p>
-                    <p><input type="text" placeholder="0" name="quantity${i}"
-                    onkeyup="checkQuantityTextbox(this);"></p>
-                </section>
-            `;
-        }
-        return str;
-    }
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path);
+    next();
 });
-app.use(express.static('./public')); 
 
-var listener = app.listen(8080, () => { console.log('server started listening on port ' + listener.address().port) });
+app.listen(8080, () => console.log(`listening on port 8080`));
